29218801d6456e3742b81e8447db27666a924a75
Joel Felldin <joelfelldin@hotmail.com>
2021-09-17 16:06:10 +0200

    Add project files.

---
+++ HittaIJulitaWeb/ViewModels/SearchViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HittaIJulitaWeb.ViewModels
+{
+    public class SearchViewModel
+    {
+        public string GatuPlats { get; set; }
+        public string Postnummer { get; set; }
+        public string Plats { get; set; }
+    }
+}
